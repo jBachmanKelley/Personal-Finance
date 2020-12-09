f1e4c0222aea3c9d41ae56a4db6010e70486b5f7
jBachmanKelley <jBachmanKelley@gmail.com>
2020-12-08 23:59:40 -0500

    Additional Statistics and Plot Features

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # This is a python script to read, analyze, and display personal finance material from a Transactions CSV
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
-import array as arr
 import math
 
 """
@@ -10,6 +8,8 @@ import math
         - Read in my Transcript CSV Data
         - Analyze the different transactions 
         - Print Statistics"""
+
+
 class Node:
     def __init__(self, value=None, index=-1):
         self.value = value
@@ -23,36 +23,46 @@ class SList:
 
     def insert(self, value):
         end = self.head
-        while end.next != None:
+        while end.next is not None:
             end = end.next
         end.next = Node(value, end.index + 1)
 
-    def getValue(self, value, mode):
+    def get_value(self, value):
         temp = self.head
         while temp.value != value and temp.next is not None:
             temp = temp.next
-        if temp.value == value and mode == 0:
+        if temp.value == value:
             return temp.index
-        elif temp.value == value and mode == 1:
-            return temp.value
         else:
             self.insert(value)
-            return self.getValue(value, mode)
+            return self.get_value(value)
+
+    def get_label(self, index):
+        temp = self.head
+        while temp.index != index and temp.next is not None:
+            temp = temp.next
+        if temp.index == index:
+            return temp.value
+        else:
+            raise ValueError
 
     def __len__(self):
         end = self.head
-        while end.next != None:
+        while end.next is not None:
             end = end.next
         return end.index + 1
 
 
 class Analyzer:
+    # noinspection PyTypeChecker
     def __init__(self):
-        self.price = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=(0))
-        self.cat = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=(1), dtype=str)
-        self.date = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=(2), dtype=str)
+
+        self.price = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=0)
+        self.cat = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=1, dtype=str)
+        self.date = np.genfromtxt('Transactions.csv', delimiter=',', skip_header=1, usecols=2, dtype=str)
         self.clean_data = np.zeros((len(self.price), 3))
         self.classes = SList('Head')
+        self.stat_str = ''  # Make stat_str a dict for each class
         self.transpose()
         self.stats()
         self.plot()
@@ -60,38 +70,55 @@ class Analyzer:
     def transpose(self):
         # Alter the Classes to  Integer values
         for i in range(self.clean_data.shape[0]):
-            self.clean_data[i][0] = -1 * math.floor(self.price[i] * 100)/ 100
-            self.clean_data[i][1] = self.convert_to_class(self.cat[i])
+            self.clean_data[i][0] = -1 * math.floor(self.price[i] * 100) / 100
+            self.clean_data[i][1] = self.convert_class_to_int(self.cat[i])
 
-    def convert_to_class(self, value):
-        return self.classes.getValue(value, 0)
+    def convert_class_to_int(self, value):
+        return self.classes.get_value(value)
+
+    def convert_int_to_class(self, value):
+        return self.classes.get_label(value)  # Needs Work
 
     def __main__(self):
         return
 
     def stats(self):
         # Sum By Category
-        sums = [0.0] * len(self.classes)
+        stats = [[0, 0, 0]] * len(self.classes) # [size, sum, mean]
         for x in self.clean_data:
-            sums[int(x[1])] += x[0]
+            x_class = int(x[1])
+            stats[x_class][0] += 1
+            stats[x_class][1] += x[0]
+            stats[x_class][2] = stats[x_class][0] / stats[x_class][1]
 
         # Print Values
         i = 0
-        for s in sums:
-            print(f'Class {i:2} Sum: ${s:8.2f}')
+        for s in stats:
+            self.stat_str += f'Class {i:2}\n    Size: {s[0]}\n    Sum: ${s[1]:8.2f}\n'
             i += 1
 
     def plot(self):
         x = self.date
         y = self.clean_data[:, 0]
         plt.scatter(x, y, c=self.clean_data[:, 1])
-        plt.title('Transactions By Date')
+        plt.suptitle('Transactions By Date')
+        plt.title('Updated: 12/07/2020', fontsize=10)  # Put a time keeper variable to track when last updated
         plt.xlabel('Date: MM/DD/YYY')
         plt.xticks(rotation=295, fontsize=6)
+        self.update_stat_box()
         plt.ylabel('Expense')
         plt.set_cmap('tab10')
         plt.show()
 
+    def update_stat_box(self):
+        plt.text(55, 200, self.stat_str, bbox={'facecolor': 'green', 'alpha': 0.4, 'pad': 15},
+                 verticalalignment='top', horizontalalignment='left')
 
 
 ob1 = Analyzer()
+
+"""
+    Notes For Improvement:
+    - Use Text.contains(self, mouseevent) to create buttons and such 
+    - Make a Dynamic statistics box 
+"""
