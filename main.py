a2b5618c42a60225e562367aea549152ff9c1fe0
jBachmanKelley <jBachmanKelley@gmail.com>
2020-12-11 22:42:32 -0500

    Debbuging A CMap Error
    The color map is skipping a color value for one of the categories while the Legend is accurately portraying them. Look at the highest 'Luxury' class for example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # This is a python script to read, analyze, and display personal finance material from a Transactions CSV
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib import colors
 import math
 
 """
@@ -14,7 +16,11 @@ class Node:
     def __init__(self, value=None, index=-1):
         self.value = value
         self.index = index
+        self.color = None
         self.next = None
+        if index != -1:
+            self.color = plt.cm.get_cmap('viridis').colors[index]
+
 
 
 class SList:
@@ -52,6 +58,20 @@ class SList:
             end = end.next
         return end.index + 1
 
+    def __iter__(self):
+        self.iterator = 1
+        return self
+
+    def __next__(self):
+        temp = self.head
+        for i in range(self.iterator):
+            temp = temp.next
+        if temp is None:
+            raise StopIteration
+        else:
+            self.iterator += 1
+            return temp
+
 
 class Analyzer:
     # noinspection PyTypeChecker
@@ -71,20 +91,14 @@ class Analyzer:
         # Alter the Classes to  Integer values
         for i in range(self.clean_data.shape[0]):
             self.clean_data[i][0] = -1 * math.floor(self.price[i] * 100) / 100
-            self.clean_data[i][1] = self.convert_class_to_int(self.cat[i])
-
-    def convert_class_to_int(self, value):
-        return self.classes.get_value(value)
-
-    def convert_int_to_class(self, value):
-        return self.classes.get_label(value)  # Needs Work
+            self.clean_data[i][1] = self.classes.get_value(self.cat[i])
 
     def __main__(self):
         return
 
     def stats(self):
         # Sum By Category
-        stats = [[0, 0, 0]] * len(self.classes) # [size, sum, mean]
+        stats = [[0] * 3 for _ in  range(len(self.classes))]  # [size, sum, mean]
         for x in self.clean_data:
             x_class = int(x[1])
             stats[x_class][0] += 1
@@ -94,20 +108,24 @@ class Analyzer:
         # Print Values
         i = 0
         for s in stats:
-            self.stat_str += f'Class {i:2}\n    Size: {s[0]}\n    Sum: ${s[1]:8.2f}\n'
+            self.stat_str += f'Class: {self.classes.get_label(i):2}\n    Size: {s[0]}\n    Sum: ${s[1]:8.2f}\n'
             i += 1
 
     def plot(self):
         x = self.date
         y = self.clean_data[:, 0]
-        plt.scatter(x, y, c=self.clean_data[:, 1])
+        legend_classes = []
+        for c in self.classes:
+            temp = mpatches.Patch(color=c.color, label=c.value)
+            legend_classes.append(temp)
+        plt.legend(handles=legend_classes)
+        plt.scatter(x, y, c=self.clean_data[:, 1], cmap='viridis')
         plt.suptitle('Transactions By Date')
         plt.title('Updated: 12/07/2020', fontsize=10)  # Put a time keeper variable to track when last updated
         plt.xlabel('Date: MM/DD/YYY')
         plt.xticks(rotation=295, fontsize=6)
         self.update_stat_box()
         plt.ylabel('Expense')
-        plt.set_cmap('tab10')
         plt.show()
 
     def update_stat_box(self):
